d2d66cef560b6aa86a1a2dbc7351affe7d257980
vickram <vickramodero6@gmail.com>
2024-07-24 16:38:26 +0300

    correct-6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,23 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, latsName, fileName) {
-  const userResponse = signUpUser(firstName, latsName);
-  const photoRespionse = uploadPhoto(fileName);
-
-  return Promise.allSettled([userResponse, photoRespionse])
-    .then((results) => {
-      results.map((result) => {
-        if (result.status === 'fulfilled') {
-          return {
-            status: 'fulfilled',
-            value: result.value,
-          };
-        }
-        return {
-          status: 'rejected',
-          value: result.reason.toString(),
-        };
-      });
-    });
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return { status: 'fulfilled', value: result.value };
+    }
+    return { status: 'rejected', value: result.reason.message };
+  }));
 }
