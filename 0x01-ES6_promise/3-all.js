98cac2e2119cf016b78fa06309908e13fbe25544
vickram <vickramodero6@gmail.com>
2024-07-24 14:52:57 +0300

    output

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup() {
 
   return Promise.all([photoResponse, userResponse])
     .then(([photo, user]) => {
-      console.log(photo.body, user.firstName, user.lastName);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     }).catch(() => {
       console.log('Signup system offline');
     });
