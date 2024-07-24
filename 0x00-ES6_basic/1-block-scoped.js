b5f6f9933cb2cdbdb7af8f57e8fdbb37cf79c9d3
vickram <vickramodero6@gmail.com>
2024-07-24 11:16:17 +0300

    seventh change

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
-  
-    if (trueOrFalse) {
-     const  task = true;
-     const task2 = false;
-    }
-  
-    return [task, task2];
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = false;
+    task2 = true;
   }
+
+  return [task, task2];
+}
