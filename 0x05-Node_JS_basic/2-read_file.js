a21147959104ba9813ce3d87041502e5c85f5762
vickram <vickramodero6@gmail.com>
2024-09-18 10:32:06 +0300

    lint fix

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,41 +1,41 @@
 const fs = require('fs');
 
 function countStudents(path) {
-    try {
-        const data = fs.readFileSync(path, 'utf-8');
-        
-        const lines = data.trim().split('\n');
-        
-        const header = lines.shift();
-        
-        if (!header) {
-            throw new Error('Cannot load the database');
-        }
-        
-        const fields = {};
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
 
-        lines.forEach((line) => {
-            const student = line.split(',');
-            if (student.length === 4) {
-                const firstName = student[0];
-                const field = student[3];
+    const lines = data.trim().split('\n');
 
-                if (!fields[field]) {
-                    fields[field] = [];
-                }
-                fields[field].push(firstName);
-            }
-        });
+    const header = lines.shift();
 
-        const totalStudents = lines.length;
-        console.log(`Number of students: ${totalStudents}`);
-
-        for (const [field, students] of Object.entries(fields)) {
-            console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
-        }
-    } catch (error) {
-        throw new Error('Cannot load the database');
+    if (!header) {
+      throw new Error('Cannot load the database');
     }
+
+    const fields = {};
+
+    lines.forEach((line) => {
+      const student = line.split(',');
+      if (student.length === 4) {
+        const firstName = student[0];
+        const field = student[3];
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstName);
+      }
+    });
+
+    const totalStudents = lines.length;
+    console.log(`Number of students: ${totalStudents}`);
+
+    for (const [field, students] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    
-    res.end('Hello Holberton School!');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
