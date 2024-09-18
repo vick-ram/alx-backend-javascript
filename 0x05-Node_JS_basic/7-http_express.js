41f09eec1d4f35cc4cba37ecaa3e71d4373c1ada
vickram <vickramodero6@gmail.com>
2024-09-18 10:37:11 +0300

    7-lint fix

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,30 +5,30 @@ const app = express();
 const PORT = 1245;
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-    const databasePath = process.argv[2];
+  const databasePath = process.argv[2];
 
-    countStudents(databasePath)
-        .then(({ totalStudents, report }) => {
-            let output = 'This is the list of our students\n';
-            output += `Number of students: ${totalStudents}\n`;
+  countStudents(databasePath)
+    .then(({ totalStudents, report }) => {
+      let output = 'This is the list of our students\n';
+      output += `Number of students: ${totalStudents}\n`;
 
-            for (const [field, names] of Object.entries(report)) {
-                output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
-            }
+      for (const [field, names] of Object.entries(report)) {
+        output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+      }
 
-            res.send(output.trim());
-        })
-        .catch(err => {
-            res.send('This is the list of our students\n' + err.message);
-        });
+      res.send(output.trim());
+    })
+    .catch((err) => {
+      res.send(`This is the list of our students\n${err.message}`);
+    });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
 
 module.exports = app;
