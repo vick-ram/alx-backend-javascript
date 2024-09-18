abf42e8e6f92e5cac89325107316bd3f8e6e84c7
vickram <vickramodero6@gmail.com>
2024-09-18 10:41:54 +0300

    lint changes on multple files

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,28 +1,26 @@
 const fs = require('fs');
 
-const readDatabase = (filePath) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.trim().split('\n').filter((line) => line !== '');
+    const fields = {};
+
+    lines.slice(1).forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+
+      if (!fields[field]) {
+        fields[field] = [];
       }
-
-      const lines = data.trim().split('\n').filter(line => line !== '');
-      const fields = {};
-
-      lines.slice(1).forEach((line) => {
-        const [firstname, , , field] = line.split(',');
-
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstname);
-      });
-
-      resolve(fields);
+      fields[field].push(firstname);
     });
+
+    resolve(fields);
   });
-};
+});
 
 module.exports = readDatabase;
